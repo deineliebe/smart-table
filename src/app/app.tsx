56e6c3a2475e01a45d362714a977c6fca5605324
deine.kleine.liebe@gmail.com <ms.balashova.2001@gmail.com>
2024-09-28 20:05:36 +0300

    Create something that can be built (replace some logic from page to app, fix format)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { useLocation, useNavigate } from "react-router";
+import React from 'react';
+//import { useDispatch } from 'react-redux';
+//import { useLocation, useNavigate } from 'react-router';
+//import Image from 'next/image';
+import styles from './page.module.css';
 
+export default function App() {
+	//const navigate = useNavigate();
+	//const location = useLocation();
+	//const background = location.state?.background;
+	//const dispatch = useDispatch();
+	//const isAuthorized = useSelector(getIsAuthorized);
 
-const App = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const background = location.state?.background;
-    const dispatch = useDispatch();
-    //const isAuthorized = useSelector(getIsAuthorized);
-  
-    /*useEffect(() => {
+	/*useEffect(() => {
       dispatch(getIngredientsList());
       if (!isAuthorized) dispatch(getUserData());
     }, []);*/
-    
-    return (
-      <div>
-      </div>
-    );
-  };
-  
-  export default App;
+
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}></main>
+			<footer className={styles.footer}>
+				<span>Alvish Baldha</span>
+				<span>© 2022 | All Rights Reserved</span>
+			</footer>
+		</div>
+	);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
-import { Provider, Provider as ReduxProvider } from 'react-redux';
-import { BrowserRouter, BrowserRouter as RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import App from './app/app';
 
@@ -9,11 +9,11 @@ const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
 );
